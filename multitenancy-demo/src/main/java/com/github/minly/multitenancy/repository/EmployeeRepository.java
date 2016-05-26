5c673bcb4815e7b166f5c790ce1fa6f385911f8a
minlywang <dolphinwang999@yahoo.com>
2016-05-26 23:35:49 +0800

    update

--- multitenancy-demo/src/main/java/com/github/minly/multitenancy/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.minly.multitenancy.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.github.minly.multitenancy.domain.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
-
-}
